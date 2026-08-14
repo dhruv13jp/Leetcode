d94484376216adab4d2b91b1d479cc9314e96cd6
Dhruv Bhatnagar <157150226+dhruv13jp@users.noreply.github.com>
2026-08-14 22:19:35 +0530

    Time: 0 ms (100.00%) | Memory: 20.4 MB (40.57%) - LeetSync

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,18 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        seen = {}
+        compliment=0
+        for i in range(len(nums)):
+            compliment = target - nums[i]
+
+            if compliment in seen:
+                return [seen[compliment], i]
+            
+            seen[nums[i]]=i
+
+
+
+
+
+        
