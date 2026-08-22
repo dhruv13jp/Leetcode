baef7cae17b44a0c3465b75d5bd28c31568744b4
Dhruv Bhatnagar <157150226+dhruv13jp@users.noreply.github.com>
2026-08-22 17:42:08 +0530

    Time: 35 ms (86.34%) | Memory: 46.8 MB (31.74%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -2,24 +2,28 @@ class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
         int n = nums.size();
-        vector<int> prefixsum(n,0);
         int count =0;
-        prefixsum[0]=nums[0];
+        vector<int> prefixsum(n,0);
+
+        prefixsum[0] = nums[0];
         for(int i=1;i<n;i++){
-            prefixsum[i] = prefixsum[i-1] + nums[i];
+            prefixsum[i] = prefixsum[i-1]+nums[i];
         }
         unordered_map<int,int> mpp;
-        for(int i=0;i<n;i++){
-            if(prefixsum[i]==k) count++;
-            int val = prefixsum[i]-k;
+        for(int j=0;j<n;j++){
+            if(prefixsum[j]==k) count++;
+
+            int val = prefixsum[j]-k;
             if(mpp.find(val)!=mpp.end()){
                 count+=mpp[val];
             }
-            if(mpp.find(prefixsum[i])==mpp.end()){
-                mpp[prefixsum[i]]=0;
+
+            if(mpp.find(prefixsum[j])==mpp.end()){
+                mpp[prefixsum[j]]=0;
             }
-            mpp[prefixsum[i]]++;
+            mpp[prefixsum[j]]++;
         }
         return count;
     }
+    
 };
